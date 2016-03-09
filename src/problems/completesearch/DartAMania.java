9aa73eca7df5e1fff4ea3b3f6270f3915cd15a60
gouthampradhan <goutham.vidya.pradhan@gmail.com>
2016-03-09 20:34:17 +0100

    DartAMania : Accepted 1.245 s. Simple bruteforce O(N^3) algorithm

---
+++ src/problems/completesearch/DartAMania.java
@@ -0,0 +1,164 @@
+package problems.completesearch;
+
+import java.io.BufferedOutputStream;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.util.Arrays;
+import java.util.BitSet;
+import java.util.StringTokenizer;
+
+/**
+ * 
+ * @author gouthamvidyapradhan
+ * Accepted 1.245 s. O(N^3) algorithm to count all the permutation and combination of throws
+ *
+ */
+public class DartAMania {
+
+    /**
+     * Scanner class
+     *
+     * @author gouthamvidyapradhan
+     */
+    static class MyScanner {
+        /**
+         * Buffered reader
+         */
+        private static BufferedReader br = new BufferedReader(
+                new InputStreamReader(System.in));
+
+        private static StringTokenizer st;
+
+        /**
+         * Read integer
+         *
+         * @return
+         * @throws Exception
+         */
+        public static int readInt() throws Exception {
+            try {
+                if (st != null && st.hasMoreTokens()) {
+                    return parseInt(st.nextToken());
+                }
+                String str = br.readLine();
+                if(str == null) return -2;
+                if (!str.trim().equals("")) {
+                    st = new StringTokenizer(str);
+                    return parseInt(st.nextToken());
+                }
+            } catch (IOException e) {
+                close();
+                return -1;
+            }
+            return -1;
+        }
+
+        /**
+         * Ready
+         * @return
+         * @throws Exception
+         */
+        public static boolean ready() throws Exception
+        {
+            return br.ready();
+        }
+
+        /**
+         * Read line
+         * @return
+         * @throws Exception
+         */
+        public static String readLine() throws Exception
+        {
+            return br.readLine();
+        }
+        /**
+         * Parse to integer
+         * @param in
+         * @return integer value
+         */
+        public static int parseInt(String in)
+        {
+            // Check for a sign.
+            int num  = 0, sign = -1, i = 0;
+            final int len  = in.length( );
+            final char ch  = in.charAt( 0 );
+            if ( ch == '-' )
+                sign = 1;
+            else
+                num = '0' - ch;
+
+            // Build the number
+            i+=1;
+            while ( i < len )
+                num = num*10 + '0' - in.charAt( i++ );
+            return sign * num;
+        }
+
+        /**
+         * Close BufferedReader
+         *
+         * @throws Exception
+         */
+        public static void close() throws Exception {
+            br.close();
+        }
+    }
+
+    private static PrintWriter pw = new PrintWriter(new BufferedOutputStream(System.out));
+    private static int S, pCount, cCount; //permutation, combination count
+    private static int[] cArray = new int[3];
+    private static BitSet done = new BitSet();
+    private static final int[] D = {0, 1, 2, 3, 4, 5, 6, 7, 21, 8, 24, 
+    	9, 27, 10, 30, 11, 22, 33, 12, 36, 13, 26, 39, 14, 28, 42, 15, 45, 16, 32, 48, 17, 34, 51, 18, 54, 19, 38, 57, 50, 20, 40, 60};
+    private static final String NOT_POSSIBLE1 = "THE SCORE OF ", NOT_POSSIBLE2 = " CANNOT BE MADE WITH THREE DARTS.", 
+    		LINE = "**********************************************************************", PERMUTATION = "NUMBER OF PERMUTATIONS THAT SCORES ", 
+    				COMBINATION = "NUMBER OF COMBINATIONS THAT SCORES ", IS = " IS ", STOP = ".", END_OUTPUT = "END OF OUTPUT";
+    
+
+    /**
+     * Main method
+     * @param args
+     * @throws Exception
+     */
+	public static void main(String[] args) throws Exception 
+	{
+		while(true)
+		{
+			S = MyScanner.readInt();
+			if(S <= 0) break;
+			int N = D.length, encode;
+			pCount = 0; cCount = 0;
+			for(int i = 0; i < N; i++)
+				for(int j = 0; j < N; j++)
+					for(int k = 0; k < N; k++)
+					{
+						if((D[i] + D[j] + D[k]) == S)
+						{
+							cArray[0] = D[i]; cArray[1] = D[j]; cArray[2] = D[k];
+							Arrays.sort(cArray); //sort
+							encode = (((cArray[0] << 6) + cArray[1]) << 6) + cArray[2];
+							if(!done.get(encode))
+							{
+								done.set(encode);
+								cCount++; 
+							}
+							pCount++;
+						}
+					}
+			if(pCount == 0)
+				pw.println(NOT_POSSIBLE1 + S + NOT_POSSIBLE2);
+			else
+			{
+				pw.println(COMBINATION + S + IS + cCount + STOP);
+				pw.println(PERMUTATION + S + IS + pCount + STOP);
+			}
+			pw.println(LINE);
+			done.clear();
+		}
+		pw.println(END_OUTPUT);
+		pw.flush(); pw.close(); MyScanner.close();
+	}
+}
