8956c3cbb3e434868b6fa037255ab2cc518e16cb
gouthampradhan <goutham.vidya.pradhan@gmail.com>
2016-03-21 00:00:02 +0100

    Safebreaker : Accepted 0.722 s brute-force O(N^4)

--- src/problems/completesearch/Safebreaker.java
+++ src/problems/completesearch/Safebreaker.java
@@ -11,6 +11,14 @@ import java.util.List;
 import java.util.Set;
 import java.util.StringTokenizer;
 
+/**
+ * 
+ * @author gouthamvidyapradhan
+ * Accepted 0.722 s.
+ * My Algorithm : O(N^4) brute-force find all possible combinations of 4-digit numbers and then, check against each guess if it satisfies each of the guess
+ * if successful then save this as the result. If there are more then 1 possible results then its indeterminate, if none found then impossible.
+ *
+ */
 public class Safebreaker {
 
     /**
@@ -108,7 +116,8 @@ public class Safebreaker {
     private static List<Integer> N = new ArrayList<>();
     private static List<Guess> guessList;
     private static Set<Integer> set;
-    private static final int NUM = 10000;
+    private static String result;
+    private static final String INDETERMINATE = "indeterminate", IMPOSSIBLE = "impossible";
     
     /**
      * Guess class
@@ -138,6 +147,7 @@ public class Safebreaker {
 			G = MyScanner.readInt();
 			guessList = new ArrayList<Guess>();
 			set = new HashSet<Integer>();
+			result = null;
 			for(int i = 0; i<G; i++)
 			{
 				String line = MyScanner.readLine();
@@ -145,8 +155,8 @@ public class Safebreaker {
 				char[] chars = String.valueOf(st.nextToken()).toCharArray();
 				int[] num = {chars[0] - '0', chars[1] - '0', chars[2] - '0', chars[3] - '0'};
 				StringTokenizer st1 = new StringTokenizer(st.nextToken(), "/");
-				int cl = Integer.parseInt(st1.nextToken());
-				int wl = Integer.parseInt(st1.nextToken());
+				int cl = MyScanner.parseInt(st1.nextToken().trim());
+				int wl = MyScanner.parseInt(st1.nextToken().trim());
 				guessList.add(new Guess(num, cl, wl));
 				if(cl == 0 && wl == 0)
 				{
@@ -158,16 +168,64 @@ public class Safebreaker {
 			for(int i = 0; i < 10; i++)
 				if(!set.contains(i))
 					N.add(i); //numbers to be used for iteration
-			set.clear(); // reuse the same set.
-			for(int a = 0, l = N.size(); a < l; a++)
-				for(int b = a; b < l; b++)
-					for(int c = b; c < l; c++)
-						for(int d = c; d < l; d++)
+			boolean indeterminate = false;
+			for(int a = 0, l = N.size(); a < l && !indeterminate; a++)
+				for(int b = 0; b < l && !indeterminate; b++)
+					for(int c = 0; c < l && !indeterminate; c++)
+						for(int d = 0; d < l && !indeterminate; d++)
+						{
+							boolean found = true;
+							int[] num = new int[4];
+							num[0] = N.get(a); num[1] = N.get(b); num[2] = N.get(c); num[3] = N.get(d);
 							for(Guess g : guessList)
 							{
+								int cl = 0, wl = 0;
+								int[] code = g.code;
+								List<Integer> temp1 = new ArrayList<>();
+								List<Integer> temp2 = new ArrayList<>();
+								if(num[0] == code[0]) cl++;
+								else {temp1.add(num[0]); temp2.add(code[0]);}
+								if(num[1] == code[1]) cl++;
+								else {temp1.add(num[1]); temp2.add(code[1]);}
+								if(num[2] == code[2]) cl++;
+								else {temp1.add(num[2]); temp2.add(code[2]);}
+								if(num[3] == code[3]) cl++;
+								else {temp1.add(num[3]); temp2.add(code[3]);}
+								for(int i = 0, l1 = temp1.size(); i < l1; i++)
+								{
+									for(int j = 0, l2 = temp2.size(); j < l2; j++)
+									{
+										if(temp1.get(i) == temp2.get(j))
+										{
+											wl++; 
+											temp2.remove(j);
+											break;
+										}
+									}
+								}
+								if(cl != g.cl || wl != g.wl)
+								{
+									found = false;
+									break;
+								}
 								//check for each guess and add to the set.
 							}
-			//if size of set > 1 then indeterminate, if empty then impossible if contains 1 then print the secret code.
+							if(found)
+							{
+								if(result == null)
+									result = "" + num[0] + num[1] + num[2] + num[3];
+								else
+								{
+									indeterminate = true;
+									break;
+								}
+							}
+						}
+			if(indeterminate) pw.println(INDETERMINATE);
+			else if(result == null) pw.println(IMPOSSIBLE);
+			else pw.println(result);
+			N.clear();
 		}
+		pw.flush(); pw.close(); MyScanner.close();
 	}
 }
