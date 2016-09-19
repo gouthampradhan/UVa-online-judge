410ff9e9f27f851b320126c8a98a309e33731373
gouthampradhan <goutham.vidya.pradhan@gmail.com>
2016-09-19 19:45:06 +0200

    CF : Cleanup

--- src/problems/codeforces/contest/B.java
+++
@@ -1,9 +0,0 @@
-package problems.codeforces.contest;
-
-/**
- * Created by gouthamvidyapradhan on 16/05/2016.
- */
-public class B
-{
-
-}
--- src/problems/codeforces/contest/TaskC.java
+++
@@ -1,9 +0,0 @@
-package problems.codeforces.contest;
-
-import problems.codeforces.contest.MyScanner;
-import java.io.PrintWriter;
-
-public class TaskC {
-    public void solve(int testNumber, MyScanner in, PrintWriter out) {
-    }
-}
