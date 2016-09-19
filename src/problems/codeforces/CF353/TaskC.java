09abc26f6da5e4ae9fba05423dc941164e6d6e07
gouthampradhan <goutham.vidya.pradhan@gmail.com>
2016-09-19 23:38:17 +0200

    Uva onine judge refactoring

--- src/problems/codeforces/CF353/TaskC.java
+++
@@ -1,9 +0,0 @@
-package problems.codeforces.CF353;
-
-import problems.codeforces.CF353.MyScanner;
-import java.io.PrintWriter;
-
-public class TaskC {
-    public void solve(int testNumber, MyScanner in, PrintWriter out) {
-    }
-}
--- src/problems/codeforces/CF355/TaskE.java
+++
@@ -1,9 +0,0 @@
-package problems.codeforces.CF355;
-
-import problems.codeforces.CF353.MyScanner;
-import java.io.PrintWriter;
-
-public class TaskE {
-    public void solve(int testNumber, MyScanner in, PrintWriter out) {
-    }
-}
